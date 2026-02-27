c715ff9ba35695763a48914008e256b8d85be636
Z4rru <123763147+Z4rru@users.noreply.github.com>
2026-02-27 22:10:58 +0800

    Update environment.hpp

--- oss-executor/src/api/environment.hpp
+++ oss-executor/src/api/environment.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <string>
+#include <functional>
+#include <unordered_map>
+#include <vector>
+
 namespace oss {
 
 class LuaEngine;
@@ -7,6 +12,17 @@ class LuaEngine;
 class Environment {
 public:
     static void setup(LuaEngine& engine);
+    static void setup_debug_lib(LuaEngine& engine);
+    static void setup_cache_lib(LuaEngine& engine);
+    static void setup_metatable_lib(LuaEngine& engine);
+    static void setup_input_lib(LuaEngine& engine);
+    static void setup_instance_lib(LuaEngine& engine);
+    static void setup_script_lib(LuaEngine& engine);
+    static void setup_websocket_lib(LuaEngine& engine);
+    static void setup_thread_lib(LuaEngine& engine);
+    static void setup_closure_lib(LuaEngine& engine);
+    static void setup_drawing_bridge(LuaEngine& engine);
+    static void setup_roblox_mock(LuaEngine& engine);
 };
 
 } // namespace oss
