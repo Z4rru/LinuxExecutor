7cdb5aedc5ea6cae9a95ea46b889a5451b214dd3
Z4rru <123763147+Z4rru@users.noreply.github.com>
2026-02-27 15:17:35 +0800

    Update environment.hpp

--- oss-executor/src/api/environment.hpp
+++ oss-executor/src/api/environment.hpp
@@ -1,44 +1,22 @@
 #pragma once
 
-#include "core/lua_engine.hpp"
+extern "C" {
+#include <luajit-2.1/lua.h>
+#include <luajit-2.1/lualib.h>
+#include <luajit-2.1/lauxlib.h>
+}
+
 #include <string>
-#include <unordered_map>
-#include <functional>
+
+// Forward declaration — LuaEngine is defined in core/lua_engine.hpp
+namespace oss { class LuaEngine; }
 
 namespace oss {
 
-// Extended Roblox environment compatibility layer
 class Environment {
 public:
+    // Sets up the full Roblox API mock + executor globals in the given engine
     static void setup(LuaEngine& engine);
-
-private:
-    static void register_globals(lua_State* L);
-    static void register_drawing_lib(lua_State* L);
-    static void register_debug_lib(lua_State* L);
-    static void register_cache_lib(lua_State* L);
-    
-    // Instance mock for standalone testing
-    static int lua_typeof(lua_State* L);
-    static int lua_checkcaller(lua_State* L);
-    static int lua_islclosure(lua_State* L);
-    static int lua_iscclosure(lua_State* L);
-    static int lua_hookfunction(lua_State* L);
-    static int lua_newcclosure(lua_State* L);
-    static int lua_getinfo(lua_State* L);
-    static int lua_getnamecallmethod(lua_State* L);
-    static int lua_setnamecallmethod(lua_State* L);
-    static int lua_getrawmetatable(lua_State* L);
-    static int lua_setrawmetatable(lua_State* L);
-    static int lua_setreadonly(lua_State* L);
-    static int lua_isreadonly(lua_State* L);
-    static int lua_getgenv(lua_State* L);
-    static int lua_getrenv(lua_State* L);
-    static int lua_getreg(lua_State* L);
-    static int lua_getgc(lua_State* L);
-    static int lua_fireclickdetector(lua_State* L);
-    static int lua_firetouchinterest(lua_State* L);
-    static int lua_fireproximityprompt(lua_State* L);
 };
 
 } // namespace oss
