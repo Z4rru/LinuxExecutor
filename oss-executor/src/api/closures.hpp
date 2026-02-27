b0903a063b4d3dc4416c2cd7d1939c22974def67
Z4rru <123763147+Z4rru@users.noreply.github.com>
2026-02-27 22:24:20 +0800

    Update closures.hpp

--- oss-executor/src/api/closures.hpp
+++ oss-executor/src/api/closures.hpp
@@ -1,11 +1,27 @@
 #pragma once
-#include <lua.hpp>
+
+extern "C" {
+#include <luajit-2.1/lua.h>
+#include <luajit-2.1/lualib.h>
+#include <luajit-2.1/lauxlib.h>
+}
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <functional>
 
 namespace oss {
 
 class Closures {
 public:
     static void register_all(lua_State* L);
+
+    struct HookEntry {
+        int original_ref = LUA_NOREF;
+        int hook_ref = LUA_NOREF;
+        std::string name;
+    };
 
 private:
     static int wrap_closure(lua_State* L);
@@ -14,6 +30,27 @@ private:
     static int compare_closures(lua_State* L);
     static int clone_function(lua_State* L);
     static int get_calling_script(lua_State* L);
+
+    static int newcclosure(lua_State* L);
+    static int newcclosure_handler(lua_State* L);
+    static int hookfunction(lua_State* L);
+    static int hookmetamethod(lua_State* L);
+    static int iscclosure(lua_State* L);
+    static int islclosure(lua_State* L);
+    static int isexecutorclosure(lua_State* L);
+    static int checkcaller(lua_State* L);
+    static int getinfo(lua_State* L);
+    static int checkclosure(lua_State* L);
+    static int getscriptclosure(lua_State* L);
+    static int loadstring_enhanced(lua_State* L);
+    static int newlclosure(lua_State* L);
+    static int newlclosure_handler(lua_State* L);
+
+    static const char* EXECUTOR_MARKER;
+    static const char* HOOK_TABLE_KEY;
+
+    static void ensure_hook_table(lua_State* L);
+    static int get_hook_table(lua_State* L);
 };
 
 } // namespace oss
