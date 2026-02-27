7cc0fa13934cf2017653069d783ce9bb2e568284
Z4rru <123763147+Z4rru@users.noreply.github.com>
2026-02-27 16:09:44 +0800

    Update closures.hpp

--- oss-executor/src/api/closures.hpp
+++ oss-executor/src/api/closures.hpp
@@ -1,18 +1,12 @@
 #pragma once
-
-extern "C" {
-#include <luajit-2.1/lua.h>
-#include <luajit-2.1/lualib.h>
-#include <luajit-2.1/lauxlib.h>
-}
+#include <lua.hpp>
 
 namespace oss {
 
-// Closure wrapping utilities for executor environment
 class Closures {
 public:
     static void register_all(lua_State* L);
-    
+
 private:
     static int wrap_closure(lua_State* L);
     static int closure_handler(lua_State* L);
