d72bb17644b2221cbe1ceb9160f18d8dce23c61d
Z4rru <123763147+Z4rru@users.noreply.github.com>
2026-02-27 19:32:49 +0800

    Update app.hpp

--- oss-executor/src/ui/app.hpp
+++ oss-executor/src/ui/app.hpp
@@ -1,17 +1,17 @@
 #pragma once
 
 #include <gtk/gtk.h>
-#include <string>
 #include <memory>
+#include <string>
 
-#include "editor.hpp"
 #include "console.hpp"
+#include "editor.hpp"
 #include "tabs.hpp"
 #include "theme.hpp"
 #include "file_dialog.hpp"
-#include "core/executor.hpp"
-#include "scripting/script_manager.hpp"
 #include "scripting/script_hub.hpp"
+#include "scripting/script_manager.hpp"
+#include "core/executor.hpp"
 
 namespace oss {
 
@@ -19,13 +19,16 @@ class App {
 public:
     App(int argc, char** argv);
     ~App();
+
+    App(const App&) = delete;
+    App& operator=(const App&) = delete;
+
     int run();
 
 private:
     void build_ui(GtkApplication* app);
     void apply_theme();
     void setup_keybinds();
-    void update_status_bar();
 
     void on_execute();
     void on_clear();
@@ -38,32 +41,32 @@ private:
     void on_toggle_console();
     void on_toggle_hub();
     void on_toggle_overlay();
+    void update_status_bar();
 
     static gboolean on_tick(gpointer data);
 
+    int argc_;
+    char** argv_;
     GtkApplication* gtk_app_ = nullptr;
     GtkWindow* window_ = nullptr;
+
+    GtkWidget* main_box_ = nullptr;
+    GtkWidget* toolbar_ = nullptr;
+    GtkWidget* paned_ = nullptr;
+    GtkWidget* sidebar_paned_ = nullptr;
+    GtkWidget* console_revealer_ = nullptr;
+    GtkWidget* hub_revealer_ = nullptr;
+    GtkWidget* status_bar_ = nullptr;
+    GtkWidget* status_label_ = nullptr;
+    GtkWidget* position_label_ = nullptr;
 
     std::unique_ptr<Editor> editor_;
     std::unique_ptr<Console> console_;
     std::unique_ptr<TabManager> tabs_;
     std::unique_ptr<ScriptHub> script_hub_;
 
-    GtkWidget* main_box_ = nullptr;
-    GtkWidget* toolbar_ = nullptr;
-    GtkWidget* paned_ = nullptr;
-    GtkWidget* status_bar_ = nullptr;
-    GtkWidget* status_label_ = nullptr;
-    GtkWidget* position_label_ = nullptr;
-    GtkWidget* sidebar_paned_ = nullptr;
-    GtkWidget* hub_revealer_ = nullptr;
-    GtkWidget* console_revealer_ = nullptr;
-
     bool console_visible_ = true;
     bool hub_visible_ = false;
-
-    int argc_;
-    char** argv_;
 };
 
 } // namespace oss
