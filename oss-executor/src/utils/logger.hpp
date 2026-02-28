c094e51a50df0ccc774cd28544de8658ebef6a67
Z4rru <123763147+Z4rru@users.noreply.github.com>
2026-03-01 00:11:22 +0800

    Update logger.hpp

--- oss-executor/src/utils/logger.hpp
+++ oss-executor/src/utils/logger.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <spdlog/spdlog.h>
 #include <string>
 
@@ -6,13 +7,28 @@ namespace oss {
 
 class Logger {
 public:
-    static void init(const std::string& log_dir = ".");
+    /// Initialize console + rotating file sinks.
+    /// Safe to call multiple times (subsequent calls are no-ops).
+    /// Returns false if file logging could not be established
+    /// (console logging will still work).
+    static bool init(const std::string& log_dir = ".");
+
+    /// Flush and tear down all spdlog loggers.
+    /// Safe to call without prior init, or multiple times.
     static void shutdown();
+
+    /// True between a successful init() and shutdown().
+    static bool initialized();
+
+private:
+    static inline bool initialized_ = false;
 };
 
 } // namespace oss
 
+// These use spdlog's fmt-style placeholders: {}
+// NOT printf-style %d / %s.
+#define LOG_DEBUG(...) spdlog::debug(__VA_ARGS__)
 #define LOG_INFO(...)  spdlog::info(__VA_ARGS__)
 #define LOG_WARN(...)  spdlog::warn(__VA_ARGS__)
 #define LOG_ERROR(...) spdlog::error(__VA_ARGS__)
-#define LOG_DEBUG(...) spdlog::debug(__VA_ARGS__)
